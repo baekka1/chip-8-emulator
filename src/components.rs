740ef0cbf7c96cae426b51d4e572093c326a6c19
baekka1 <baekka@bc.edu>
2025-06-10 21:36:58 -0500

    the beginnings

---
+++ src/components.rs
@@ -0,0 +1,18 @@
+pub struct VariableRegisters {
+    v0: u8,
+    v1: u8,
+    v2: u8,
+    v3: u8,
+    v4: u8, 
+    v5: u8, 
+    v6: u8,
+    v7: u8,
+    v8: u8,
+    v9: u8,
+    vA: v8,
+    vB: v8,
+    vC: v8, 
+    vD: v8,
+    vE: v8,
+    vF: v8
+}
