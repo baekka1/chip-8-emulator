ea07b540e01b7476c6af6d0d48f70e48177223b5
Katie Baek <baekka@bc.edu>
2025-08-15 23:13:43 -0700

    window display

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-let const FONTSET: [u8; 80] = [
+mod components;
+
+use minifb::{Key, Window, WindowOptions};
+
+const WIDTH: usize = 64;
+const HEIGHT: usize = 32;
+
+const FONTSET: [u8; 80] = [
     0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
     0x20, 0x60, 0x20, 0x20, 0x70, // 1
     0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
@@ -14,7 +21,7 @@ let const FONTSET: [u8; 80] = [
     0xF0, 0x80, 0x80, 0x80, 0xF0, // C
     0xE0, 0x90, 0x90, 0x90, 0xE0, // D
     0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
 ];
 
 fn load_fontset_into_memory(memory: &mut [u8; 4096]) {
@@ -25,14 +32,40 @@ fn load_fontset_into_memory(memory: &mut [u8; 4096]) {
     }
 }
 
-
-
 fn main() {
+    let mut stack = Vec::new(); // the stack
+
     let mut mem = [0u8; 4096]; // allocated on the stack
-    let pc: u16 = 0;
-    let s_timer : u8 = 0; // sound timer
-    let d_timer : u8 = 0; // delay timer
+    let pc: u16 = 512; // program counter - starts at 512 bc that's where the program will get
+    // loaded into memory
+    let s_timer: u8 = 0; // sound timer
+    let d_timer: u8 = 0; // delay timer
 
     // Load the fontset into memory
     load_fontset_into_memory(&mut mem);
+
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+
+    println!("Creating window");
+    let mut window = Window::new(
+        "Test - ESC to exit",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|e| {
+        panic!("Reached an error: {}", e);
+    });
+
+    // Limit to max ~60 fps update rate
+    window.set_target_fps(60);
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for i in buffer.iter_mut() {
+            *i = 1;
+        }
+
+        // unwrap here as we want this code to exit if it fails
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+    }
 }
