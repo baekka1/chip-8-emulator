0b03f67b45118cf2393ade56c1cdef845b83ec2c
Katie Baek <baekka@bc.edu>
2025-11-25 14:52:40 -0500

    fetch and decode

--- src/emulator/cpu.rs
+++ src/emulator/cpu.rs
@@ -1,8 +1,19 @@
+use crate::emulator::Memory;
+
 pub struct Cpu {
     pub stack: [u16; 1024],
     pub gen_registers: [u8; 16],
     pub pc: u16,
     pub i: u16,
+}
+
+struct Opcode {
+    first_nibble: u8,
+    x: u8,
+    y: u8,
+    n: u8,
+    nn: u8,
+    nnn: u16,
 }
 
 impl Cpu {
@@ -13,5 +24,34 @@ impl Cpu {
             pc: 0,
             i: 0,
         }
+    }
+
+    pub fn fetch(&mut self, memory: &Memory) -> u16 {
+        let first_byte = memory.data[self.pc as usize] as u16;
+        let second_byte = memory.data[(self.pc + 1) as usize] as u16;
+
+        let opcode = (first_byte << 8) | second_byte;
+
+        self.pc += 2;
+
+        return opcode;
+    }
+
+    pub fn decode(&mut self, opcode: u16) {
+        let first_nibble = ((opcode & 0xF000) >> 12) as u8;
+        let x = ((opcode & 0x0F00) >> 8) as u8;
+        let y = ((opcode & 0x00F0) >> 4) as u8;
+        let n = (opcode & 0x000F) as u8;
+        let nn = (opcode & 0x00FF) as u8;
+        let nnn = opcode & 0x0FFF as u16;
+
+        let opcode = Opcode {
+            first_nibble: first_nibble,
+            x: x,
+            y: y,
+            n: n,
+            nn: nn,
+            nnn: nnn,
+        };
     }
 }
